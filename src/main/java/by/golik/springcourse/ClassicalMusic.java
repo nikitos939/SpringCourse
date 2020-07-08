90d51280d706ce872e11bfd4bb610308b9e2339d
Nikita Golik <nikitq4a@gmail.com>
2020-07-08 21:50:19 +0300

    First commit

---
+++ src/main/java/by/golik/springcourse/ClassicalMusic.java
@@ -0,0 +1,11 @@
+package by.golik.springcourse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/by/golik/springcourse/Music.java
@@ -0,0 +1,5 @@
+package by.golik.springcourse;
+
+public interface Music {
+    String getSong();
+}
