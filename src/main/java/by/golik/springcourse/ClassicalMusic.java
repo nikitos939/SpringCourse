fe70626421d18b148dc4c6e233fcaf2e5055a47a
Nikita Golik <nikitq4a@gmail.com>
2020-07-10 18:17:28 +0300

    Scope, Value, PostConstruct, PreDestroy

--- src/main/java/by/golik/springcourse/ClassicalMusic.java
+++ src/main/java/by/golik/springcourse/ClassicalMusic.java
@@ -1,9 +1,24 @@
 package by.golik.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public  void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destroy");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/by/golik/springcourse/TestSpring.java
+++ src/main/java/by/golik/springcourse/TestSpring.java
@@ -24,8 +24,14 @@ public class TestSpring {
 //        musicPlayer.playMusic();
 
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+//        Computer computer = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
 
         context.close();
     }
